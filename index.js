6e3635f6060a92e2ab920088e7aae02f52138d62
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-10-07 14:08:12 +0100

    input for pgen with css styling

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const slider = document.getElementById('lengthSlider');
 const sliderValue = document.getElementById('sliderValue');
 const myForm = document.getElementById('myForm');
+const upper = document.getElementById('upper');
+const lower = document.getElementById('lower');
+const symbol = document.getElementById('sym');
+const number = document.getElementById('num');
+
+
 
 function updateSlider() {
   const val = slider.value;
@@ -15,5 +21,19 @@ updateSlider();
 
 myForm.addEventListener("submit", function(event){
   event.preventDefault();
-  
+  if(upper.checked){
+    console.log(upper.value);
+  }
+  if(lower.checked){
+    console.log(lower.value);
+  }
+  if(number.checked){
+    console.log(number.value);
+  }
+  if(symbol.checked){
+    console.log(symbol.value);
+  }
+
+  myForm.reset();
+
 });
